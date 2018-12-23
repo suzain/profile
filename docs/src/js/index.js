fdb0f079a1a1c3943c27e5bc17d1f3c285787362
unknown <suzainekhursheed@gmail.com>
2018-12-23 14:28:08 +0530

    adding source file

---
+++ docs/src/js/index.js
@@ -0,0 +1,28 @@
+function loadHome(){
+    const test =  document.getElementById("article-body");
+    test.innerHTML= `<p>About me.</p>
+    <p></p>`;
+}
+
+function loadAboutMe(){
+    const test =  document.getElementById("article-body");
+    test.innerHTML= `<p></p>
+    <p>Hobbies:</p>
+    <ul>
+    <li>Learning new technologies</li>
+    <li>Reading tech blogs</li>
+    <li>Fascinated about mobile operating systems</li>
+    <li>Love Music</li>
+    <li>Playing badminton</li>
+    </ul>`;
+}
+
+function loadContact(){
+    const test =  document.getElementById("article-body");
+    test.innerHTML =`<p>Suzain</p>
+    <p>Banaswadi</p>
+    <p>Bangalore</p>
+    <p>India</p>
+    <p>560043</p>`;
+    
+}
